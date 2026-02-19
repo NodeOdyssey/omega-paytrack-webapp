5032041098bdcf859d2596ff627d4f1acef9b095
Achinta Sharma <23achinta@gmail.com>
2026-02-19 18:31:34 +0530

    fix: add api.ts

---
+++ src/configs/api.ts
@@ -0,0 +1,15 @@
+export const api = {
+  // baseUrl: 'http://localhost:8086/paytrack/api/v1',
+  baseUrl: 'https://omegapaytrack.com/paytrack/api/v1',
+  login: '/auth/login',
+  logout: '/auth/logout',
+  sendPassVerificationEmail: '/auth/send-reset-password-email',
+  verifyAuthToken: '/auth/verify',
+  setNewPassword: '/auth/set-new-password',
+  ranks: '/ranks',
+  posts: '/posts',
+  employees: '/employees',
+  attendance: '/attendance',
+  reports: '/reports',
+};
+
